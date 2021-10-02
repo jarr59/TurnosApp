08c02f47dc96d2c62f7d1c6e9da3abeb577ab37b
Jarr59 <rrjosearr@gmail.com>
2021-10-02 10:50:14 -0600

    primer commit

---
+++ Models/TurnosContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Turnos.Models
+{
+    public class TurnosContext : DbContext
+    {
+        public TurnosContext(DbContextOptions<TurnosContext> options) : base (options)
+        {}
+        public DbSet<Especialidad> Especialidad { get; set; }
+    }
+}
